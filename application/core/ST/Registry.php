c9bae2c12230619518af30088fdb29a31c5fabf9
jmi <aarefiev@simtechdev.com>
2014-01-04 16:56:43 +0400

    Changes on get and set functions

--- application/core/ST/Registry.php
+++ application/core/ST/Registry.php
@@ -14,30 +14,56 @@ abstract class Registry {
 }
 
 class VariablesRegistry extends Registry {
-    private $values = array();
+    private $_values = array();
     private static $_instance;
     
     private function __construct() { }
+    private function __clone() { }
     
     static function instance() {
-        if ( ! isset(self::$_instance) ) {
+        if ( ! isset( self::$_instance ) ) {
             self::$_instance = new self();
         }
         return self::$_instance;
     }
     
-    protected function _get( $key ) {
-        if ( isset( $this->values[$key] ) ) {
-            return $this->values[$key];
+    protected function _get( $key ) {        
+        $keys = explode('.', $key);
+        $result = $this->_values;
+        
+        foreach ($keys as $k) {
+            if ( isset( $result[$k] ) ) {
+                $result = $result[$k];
+            }
         }
-        return NULL;
+        
+        return !empty($result) ? $result : NULL;
     }
     
     protected function _set( $key, $value ) {
-        $this->values[$key] = $value;
+        $keys = explode('.', $key);
+        $result = $_result = array();
+        $first_step = true;
+        $i = 0;
+        
+        foreach ($keys as $k) {
+            $i++;
+            if ( $first_step ) {
+                $_result = &$result[$k];
+                $first_step = !$first_step;
+            } else {
+                $_result = &$_result[$k];
+            }
+            
+            if ( $i == count( $keys ) ) {
+                $_result = $value;
+            }
+        }
+        
+        $this->_values = array_merge_recursive($this->_values, $result);
     }
     
-    protected function _del ( $key ) {
+    protected function _del( $key ) {
         if ( isset( $this->values[$key] ) ) {
             unset($this->values[$key]);
             return true;
